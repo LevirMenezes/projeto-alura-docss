95465963258b2707e10ac8177559fd5e1d67ca1f
Levir Menezes <54421490+LevirMenezes@users.noreply.github.com>
2023-04-25 11:28:55 -0400

    Add files via upload

---
+++ public/socket-front-documento.js
@@ -0,0 +1,27 @@
+import { alertarERedirecionar, atualizarTextoEditor } from "./documento.js";
+
+const socket = new io();
+
+function selecionarDocumento(nome) {
+  socket.emit("selecionar_documento", nome, (texto) => {
+    atualizarTextoEditor(texto);
+  });
+}
+
+function emitirTextoEditor(dados){
+  socket.emit("texto_editor", dados);
+}
+
+socket.on("texto_editor_clientes", (texto) => {
+  atualizarTextoEditor(texto);
+})
+
+function emitirExcluirDocumento(nome) {
+  socket.emit("excluir_documento", nome);
+}
+
+socket.on("excluir_documento_sucesso", (nome) => {
+  alertarERedirecionar(nome);
+});
+
+export { emitirTextoEditor, selecionarDocumento, emitirExcluirDocumento };
